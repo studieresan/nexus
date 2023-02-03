7e5f9d5c875f0359796a8bbf7b8f17141a33d597
William Bigert <wille555@hotmail.se>
2023-02-03 17:43:04 +0100

    footer added collaborations

--- src/features/Footer/index.jsx
+++ src/features/Footer/index.jsx
@@ -1,8 +1,35 @@
 import { BsFacebook, BsInstagram, BsLinkedin } from 'react-icons/bs'
 import studsLogo from '@/assets/images/Logo_ROUND.png'
+import goldman_sachs from '@/assets/images/goldman_sachs.png'
+import karma from '@/assets/images/karma.png'
+import scania from '@/assets/images/scania.png'
+import storykit from '@/assets/images/storykit.png'
+import fra from '@/assets/images/fra.png'
+import { useTranslation } from 'react-i18next'
+
 export default function Footer () {
+  const { t, i18n } = useTranslation()
+
   return (
     <div className='container-fluid p-4'>
+      <div className='text-center'>
+        <h1 className='fw-light'>{t('footer.collaborations')}</h1>
+      </div>
+      <div className='row gap-4 row-cols-1 p-4'>
+        <div className='col'>
+          <div className='row justify-content-center'>
+            <img src={goldman_sachs} alt='Goldman Sachs' className='col-1 img-fluid' />
+            <img src={karma} alt='Karma' className='col-1 mg-fluid' />
+            <img src={scania} alt='Scania' className='col-1 img-fluid' />
+          </div>
+        </div>
+        <div className='col'>
+          <div className='row justify-content-center'>
+            <img src={storykit} alt='Storykit' className='col-1 img-fluid' />
+            <img src={fra} alt='FRA' className='col-1 img-fluid' />
+          </div>
+        </div>
+      </div>
       <div className='row row-cols-1 col-12 justify-content-center'>
         <hr className='col-9 opacity-25' style={{ height: 1, opacity: 1 }} />
       </div>
