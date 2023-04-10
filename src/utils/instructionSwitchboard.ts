144dabfcae185df7a796b2fa36c8c38a84234a56
William Bigert <wille555@hotmail.se>
2023-04-10 15:45:30 +0200

    author fix

--- src/utils/instructionSwitchboard.ts
+++ src/utils/instructionSwitchboard.ts
@@ -1,8 +1,8 @@
 import { InstructionArgs, InstructionData } from '@/models/Instruction'
 import { createBlogPost, deleteBlogpost, loginUser, updateEvent, createEvent, updateBlogPost, requestPasswordReset, resetPassword, createUser, updateUser, removeEventWithId, deleteUser } from '@/requests/api'
 import { setLoggedIn, setLoggedOut } from '@/requests/auth'
-import { BlogPost } from '@/models/BlogPost';
-import { EventPost } from '@/models/EventPost';
+import { BlogPost, BlogPostStringifiedAuthor } from '@/models/BlogPost';
+import { EventPost, EventPostStringifedAuthor } from '@/models/EventPost';
 import { LoginResponse } from '@/models/Login';
 import { assertDefined } from './assertDefined';
 import { User } from '@/models/User';
@@ -14,22 +14,33 @@ export default async function instructionSwitchboard (args: InstructionArgs, ins
   switch (instruction) {
     case 'updateBlogPost': {
       const blogPost = assertDefined(data.blogPost, instruction, 'data.blogPost');
-      const response: BlogPost = await updateBlogPost(blogPost);
-      response.date = new Date(response.date)
-      console.log('new post response: ', response)
+      const response: BlogPostStringifiedAuthor = await updateBlogPost(blogPost);
 
-      const newBlogPosts: BlogPost[] = (args.appData.blogPosts || []).map((post: BlogPost) => post.id === blogPost.id ? response : post)
+      const newDate = new Date(response.date)
+      const newAuthor: User | undefined = (args.appData.users || []).find((user: User) => user.id === response.author)
+      if (!newAuthor) {
+        throw new Error('Could not find author for new blog post')
+      }
+      
+      const newPost: BlogPost = { ...response, date: newDate, author: newAuthor }
+      const newBlogPosts: BlogPost[] = (args.appData.blogPosts || []).map((post: BlogPost) => post.id === blogPost.id ? newPost : post)
       const sortedPosts = newBlogPosts.sort((a: BlogPost, b: BlogPost) => a.date < b.date ? 1 : -1)
-      args.setAppData({ ...args.appData, blogPosts: newBlogPosts })
+      
+      args.setAppData({ ...args.appData, blogPosts: sortedPosts })
       break
     }
     case 'createBlogPost': {
       const blogPost = assertDefined(data.blogPost, instruction, 'data.blogPost');
-      const response: BlogPost = await createBlogPost(blogPost)
-      response.date = new Date(response.date)
+      const response: BlogPostStringifiedAuthor = await createBlogPost(blogPost)
+      const newDate = new Date(response.date)
+      const newAuthor: User | undefined = (args.appData.users || []).find((user: User) => user.id === response.author)
+      if (!newAuthor) {
+        throw new Error('Could not find author for new blog post')
+      }
+      const newPost: BlogPost = { ...response, date: newDate, author: newAuthor }
       console.log('new post response: ', response)
 
-      const newBlogPosts: BlogPost[] = [response, ...(args.appData.blogPosts || [])]
+      const newBlogPosts: BlogPost[] = [newPost, ...(args.appData.blogPosts || [])]
       const sortedPosts = newBlogPosts.sort((a: BlogPost, b: BlogPost) => a.date < b.date ? 1 : -1)
       args.setAppData({ ...args.appData, blogPosts: sortedPosts })
       break
@@ -45,19 +56,32 @@ export default async function instructionSwitchboard (args: InstructionArgs, ins
     }
     case 'updateEventPost': {
       const eventToUpdate = assertDefined(data.eventPost, instruction, 'data.eventPost');
-      const response: EventPost = await updateEvent(eventToUpdate)
-      response.date = new Date(response.date)
+      const response: EventPostStringifedAuthor = await updateEvent(eventToUpdate)
       
-      const newPosts = (args.appData.events || []).map((post: EventPost) => post.id === eventToUpdate.id ? response : post)
+      const newDate = new Date(response.date)
+      const newAuthor = (args.appData.users || []).find((user: User) => user.id === response.author)
+      if (!newAuthor) {
+        throw new Error('Could not find author for new event post')
+      }
+      const newPost: EventPost = { ...response, date: newDate, author: newAuthor }
+      
+      const newPosts = (args.appData.events || []).map((post: EventPost) => post.id === eventToUpdate.id ? newPost : post)
       const sortedPosts = newPosts.sort((a: EventPost, b: EventPost) => a.date < b.date ? 1 : -1)
       args.setAppData({ ...args.appData, events: sortedPosts })
       break
     }
     case 'createEventPost': {
       const eventToCreate = assertDefined(data.eventPost, instruction, 'data.eventPost');
-      const response: EventPost = await createEvent(eventToCreate)
-      response.date = new Date(response.date)
-      const newPosts = [response, ...(args.appData.events || [])]
+      const response: EventPostStringifedAuthor = await createEvent(eventToCreate)
+
+      const newDate = new Date(response.date)
+      const newAuthor = (args.appData.users || []).find((user: User) => user.id === response.author)
+      if (!newAuthor) {
+        throw new Error('Could not find author for new event post')
+      }
+      const newPost: EventPost = { ...response, date: newDate, author: newAuthor }
+
+      const newPosts = [newPost, ...(args.appData.events || [])]
       const sortedPosts = newPosts.sort((a: EventPost, b: EventPost) => a.date < b.date ? 1 : -1)
       args.setAppData({ ...args.appData, events: sortedPosts })
       break
