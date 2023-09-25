0b709ef9b531a8ad78e4b63fc830eed0b276fec9
Lucas Dow <lucdow7@gmail.com>
2023-09-25 11:29:35 +0200

    Improved language diversity of call to action contact sales

--- src/features/Homepage/components/IntroSection.tsx
+++ src/features/Homepage/components/IntroSection.tsx
@@ -214,7 +214,9 @@ export function IntroSection({
         }}
       >
         <div>
-          <h2 className="text-white text-center">Interested in Studs 2024?</h2>
+          <h2 className="text-white text-center">
+            {t("homepage.callToAction.text")}
+          </h2>
         </div>
         <div className="w-100 text-center">
           <button className="btn btn-primary btn-lg">
@@ -224,7 +226,9 @@ export function IntroSection({
               rel="noopener noreferrer"
               target="_blank"
             >
-              <h2 className="text-white text-center">Contact Sales</h2>
+              <h2 className="text-white text-center">
+                {t("homepage.callToAction.button")}
+              </h2>
             </a>
           </button>
         </div>
